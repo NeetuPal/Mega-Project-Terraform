9df2c34791bdc26dbdf96110cf91c166ace9c715
Neetu Pal <65500136+NeetuPal@users.noreply.github.com>
2025-12-03 18:30:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -96,6 +96,14 @@ resource "aws_eks_cluster" "devopsshack" {
   }
 }
 
+#-------
+resource "aws_eks_addon" "ebs_csi_driver" {
+  cluster_name             = aws_eks_cluster.devopsshack.name
+  addon_name               = "aws-ebs-csi-driver"
+  service_account_role_arn = aws_iam_role.ebs_csi_driver.arn
+}
+
+#----------
 /*
 resource "aws_eks_addon" "ebs_csi_driver" {
   cluster_name    = aws_eks_cluster.devopsshack.name
